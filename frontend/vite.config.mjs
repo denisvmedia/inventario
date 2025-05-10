58a7586d605ad00e63fe0fb532ab43f34a7c426a
Denis Voytyuk <5462781+denisvmedia@users.noreply.github.com>
2025-05-10 22:58:49 +0200

    Fix vite config (#116)

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -11,16 +10,11 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
-    open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3333',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        changeOrigin: true
       }
     }
-  },
-  // Ensure the base path is correctly set
-  base: '/'
+  }
 })
