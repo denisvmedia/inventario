c0721af66c158d3d923bb5d9c257ef68a4e091b6
Denis V <5462781+denisvmedia@users.noreply.github.com>
2025-05-18 17:21:30 +0200

    Fix broken pdf.js

--- frontend/src/utils/pdfjs-init.ts
+++ frontend/src/utils/pdfjs-init.ts
@@ -1,7 +1,8 @@
 import * as pdfjsLib from 'pdfjs-dist'
+import pdfWorkerUrl from 'pdfjs-dist/build/pdf.worker.min.mjs?url';
 
 // Set the worker source to use the local worker file
 // This is more reliable than using a CDN and works offline
-pdfjsLib.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js'
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorkerUrl
 
 export { pdfjsLib }
