61a892bd6a765c6c2c962f920c517f31d95cabdc
Denis V <5462781+denisvmedia@users.noreply.github.com>
2025-08-19 22:06:12 +0200

    Add missing main.go

---
+++ go/main.go
@@ -0,0 +1,14 @@
+package main
+
+// @title Inventario API
+// @version 1.0
+// @description This is an Inventario daemon.
+
+// @contact.name Inventario Support
+// @contact.url https://github.com/denisvmedia/inventario/issues
+// @contact.email ask@artprima.cz
+
+// @license.name MIT
+
+// @BasePath /api/v1
+func main() {}
