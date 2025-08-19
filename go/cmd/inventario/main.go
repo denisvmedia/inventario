ee9981ca6c710db2afd2b9ea1b0f6a59d95e2983
Denis V <5462781+denisvmedia@users.noreply.github.com>
2025-08-19 22:06:35 +0200

    Add remove unused lines

--- go/cmd/inventario/main.go
+++ go/cmd/inventario/main.go
@@ -6,18 +6,6 @@ import (
 	"github.com/denisvmedia/inventario/registry/postgres"
 )
 
-// @title Inventario API
-// @version 1.0
-// @description This is an Inventario daemon.
-
-// @contact.name Inventario Support
-// @contact.url https://github.com/denisvmedia/inventario/issues
-// @contact.email ask@artprima.cz
-
-// @license.name MIT
-
-// @BasePath /api/v1
-
 func registerDBBackends() (cleanup func() error) {
 	// Register backends with the traditional registry system
 	boltdb.Register()
